94527f82a18923d266225acff00a9eab946eb88b
Pavaroti707 <hasicmahir@gmail.com>
2021-03-20 00:32:29 +0100

    Day12

---
+++ ex04/src/index.js
@@ -0,0 +1,23 @@
+// Only change code below this line
+function sumFibonacci(num) {
+  var prevNumber = 0;
+  var currNumber = 1;
+  var result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+// Only change code above this line
+console.log(sumFibonacci(1)); // Change this line
+console.log(sumFibonacci(10)); // Change this line
+console.log(sumFibonacci(20)); // Change this line
+console.log(sumFibonacci(4)); // Change this line
+console.log(sumFibonacci(-5)); // Change this line
+module.exports = sumFibonacci;
